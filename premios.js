14e760ea32b8c5f2f6d045b9987b128bb05e60f9
magali-prog <mauramagalilujan@gmail.com>
2026-07-07 17:51:17 -0300

    Add files via upload

---
+++ premios.js
@@ -0,0 +1,23 @@
+const premios = [
+
+"🎨 Flyer Personalizado + 5 Stickers",
+
+"📢 Publicidad por 1 Día + 1 Flyer a eleccion",
+
+"👤 Perfil Personalizado + Marca de agua + 2 Stickers",
+
+"🖼️ Flyer a Elección + 2 Stickers",
+
+"💬 Pack de 10 Stickers",
+
+"📋 Cronograma Personalizado + Marca de agua",
+
+"📱 Fondo para WhatsApp + 3 Stickers",
+
+"💧 Marca de Agua + Perfil + 3 Stickers",
+
+"🎨 2 Flyer + Fondo para WhatsApp",
+
+"✨ Perfil + Pack de 5 Stickers"
+
+];
